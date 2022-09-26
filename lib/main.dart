c77c31a9a3c365c35a3c3322fecbebb62d3db8fe
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-26 12:39:19 +0500

    Using get method to  photo api by creating own model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_api/photos_screen.dart';
 
 import 'home_screen.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: PhotoScreen(),
     );
   }
 }
